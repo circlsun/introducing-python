df016f611acfea6c4117ef2ca69b8ad808aa2495
Matveev Andrey <circlsun@gmail.com>
2023-01-21 23:31:02 +0300

    Add works()

--- ch9/apps.py
+++ ch9/apps.py
@@ -3,8 +3,14 @@ def buggy(arg, result=[]):
     return result
 
 
+def works(arg):
+    result = []
+    result.append(arg)
+    return result
+
+
 def nonbuggy(arg, result=[]):
-    if not result:
+    if result is None:
         result = []
     result.append(arg)
     return result
@@ -14,7 +20,12 @@ print(buggy(5))
 print(buggy(6))
 print(buggy(7))
 print(buggy(8))
-
+print()
+print(works(5))
+print(works(6))
+print(works(7))
+print(works(8))
+print()
 print(nonbuggy(5))
 print(nonbuggy(6))
 print(nonbuggy(7))
